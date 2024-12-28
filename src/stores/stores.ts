7a5b1d012efd1668687f4e3b226d6b6bc19dc5e6
Adam Pelc <sikulin94@gmail.com>
2024-12-28 16:23:15 +0200

    .

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,11 +1,21 @@
-let stores
+import ReactDOM from "react-dom";
+import React from "react";
+import { Store, Stores, StoreState } from "./StoresTypes";
 
-const useStore = () => {
+let stores: Stores = {};
+let storeId = 0;
 
-}
+const useStore = () => {};
 
-const createStore = () => {
-    
-}
+const createStore = <State extends StoreState>(initialState: State) => {
+  const id = storeId++;
+  let store: Store<State> =  {
+    state: initialState,
+    getState: () => store.state,
+    setState: (state) => (store.state = typeof state === "function" ? state(store.state) : state),
+  };
 
-export default createStore
+  stores[id] = store as Store<StoreState>
+};
+
+export default createStore;
